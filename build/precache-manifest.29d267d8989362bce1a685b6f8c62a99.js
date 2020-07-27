7e0bbf1dc95727d04094097d374c40039479d5b5
jfelici5 <jfelici5@u.rochester.edu>
2020-07-27 09:11:27 -0700

    fixing stuff

---
+++ build/precache-manifest.29d267d8989362bce1a685b6f8c62a99.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc75ef87d18bad31312ae1d82af12c6",
+    "url": "/jfelici5.github.io/afterhours/index.html"
+  },
+  {
+    "revision": "188f452f43df84a1b457",
+    "url": "/jfelici5.github.io/afterhours/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "509abcbff2e3bd323103",
+    "url": "/jfelici5.github.io/afterhours/static/css/main.48d31000.chunk.css"
+  },
+  {
+    "revision": "188f452f43df84a1b457",
+    "url": "/jfelici5.github.io/afterhours/static/js/2.d0a10b57.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/jfelici5.github.io/afterhours/static/js/2.d0a10b57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509abcbff2e3bd323103",
+    "url": "/jfelici5.github.io/afterhours/static/js/main.a0b08e70.chunk.js"
+  },
+  {
+    "revision": "b2a46335c47b6a280e38",
+    "url": "/jfelici5.github.io/afterhours/static/js/runtime-main.46c4376a.js"
+  },
+  {
+    "revision": "e7d4bf84ce1615a4ade3a1af0da01a1d",
+    "url": "/jfelici5.github.io/afterhours/static/media/acid-01.e7d4bf84.svg"
+  },
+  {
+    "revision": "5eb235b709554e9de55a4c03456162e2",
+    "url": "/jfelici5.github.io/afterhours/static/media/ashwin.5eb235b7.jpeg"
+  },
+  {
+    "revision": "e63df78377fc9383d54c6652ae95c65e",
+    "url": "/jfelici5.github.io/afterhours/static/media/banner.e63df783.JPG"
+  },
+  {
+    "revision": "0d239297b68efeacd38d7a9d13b7e537",
+    "url": "/jfelici5.github.io/afterhours/static/media/bellyache-01.0d239297.svg"
+  },
+  {
+    "revision": "47838c585ef0e74a6f12d830846af28f",
+    "url": "/jfelici5.github.io/afterhours/static/media/ben.47838c58.jpeg"
+  },
+  {
+    "revision": "990b8d0fd0432a13e313abd6de7be269",
+    "url": "/jfelici5.github.io/afterhours/static/media/bestpart-01.990b8d0f.svg"
+  },
+  {
+    "revision": "48f5b924f83c36006ac517b71829c449",
+    "url": "/jfelici5.github.io/afterhours/static/media/brookyln-01.48f5b924.svg"
+  },
+  {
+    "revision": "4851b811176b39b696d42ab78dc2e752",
+    "url": "/jfelici5.github.io/afterhours/static/media/candid-rettner.4851b811.jpg"
+  },
+  {
+    "revision": "f4cb811a05e5588d7db8837be6b369d3",
+    "url": "/jfelici5.github.io/afterhours/static/media/cough-01.f4cb811a.svg"
+  },
+  {
+    "revision": "13b5fe797e42acf0b3f5e2cef58aa63e",
+    "url": "/jfelici5.github.io/afterhours/static/media/dance-01.13b5fe79.svg"
+  },
+  {
+    "revision": "fb165aa67deea3fd7549fd9382c8ca5a",
+    "url": "/jfelici5.github.io/afterhours/static/media/enma.fb165aa6.jpeg"
+  },
+  {
+    "revision": "f91484ceec1301c999ca199f92945d00",
+    "url": "/jfelici5.github.io/afterhours/static/media/erin.f91484ce.jpeg"
+  },
+  {
+    "revision": "34e9a5ba3b420682a6a919eee5b86d05",
+    "url": "/jfelici5.github.io/afterhours/static/media/finallogopng.34e9a5ba.png"
+  },
+  {
+    "revision": "d4270da38c03ced98dbf25bc94c197bf",
+    "url": "/jfelici5.github.io/afterhours/static/media/franklin.d4270da3.jpeg"
+  },
+  {
+    "revision": "e68989fde6f7e9de2e17c920b506c00d",
+    "url": "/jfelici5.github.io/afterhours/static/media/grace.e68989fd.jpeg"
+  },
+  {
+    "revision": "9720a01378d15ccfdcd9dad6c6e8f5cf",
+    "url": "/jfelici5.github.io/afterhours/static/media/groovestand.9720a013.jpg"
+  },
+  {
+    "revision": "cb11fa7a45c80d9fe766f11ac63b9444",
+    "url": "/jfelici5.github.io/afterhours/static/media/hands-up.cb11fa7a.jpg"
+  },
+  {
+    "revision": "67e598e175113cbbf676cb4412e9df61",
+    "url": "/jfelici5.github.io/afterhours/static/media/heart-01.67e598e1.svg"
+  },
+  {
+    "revision": "e194403b1926c254d390be4df4a92164",
+    "url": "/jfelici5.github.io/afterhours/static/media/jack.e194403b.jpeg"
+  },
+  {
+    "revision": "a803783b5d9298ca1885359bb0608ef4",
+    "url": "/jfelici5.github.io/afterhours/static/media/japanreal-01.a803783b.svg"
+  },
+  {
+    "revision": "8c21ee49440d8af4b4ed4fd636e10875",
+    "url": "/jfelici5.github.io/afterhours/static/media/juice-01.8c21ee49.svg"
+  },
+  {
+    "revision": "48696ec82cac4627f561bbc1fec254f1",
+    "url": "/jfelici5.github.io/afterhours/static/media/luke.48696ec8.jpeg"
+  },
+  {
+    "revision": "27e35bd46a86db8bd503a1934efaf2a8",
+    "url": "/jfelici5.github.io/afterhours/static/media/more.27e35bd4.svg"
+  },
+  {
+    "revision": "72205700856e4f3d1eadf0b6e3c42742",
+    "url": "/jfelici5.github.io/afterhours/static/media/soclose-01.72205700.svg"
+  },
+  {
+    "revision": "08e6542c259b29abdf30d093dc43828a",
+    "url": "/jfelici5.github.io/afterhours/static/media/steven.08e6542c.jpeg"
+  },
+  {
+    "revision": "df4fb538e7a668dc346b4283f3e397c8",
+    "url": "/jfelici5.github.io/afterhours/static/media/sydney.df4fb538.jpeg"
+  },
+  {
+    "revision": "ea98352bf7addf24a59e9d06b0077e9b",
+    "url": "/jfelici5.github.io/afterhours/static/media/treasure-01.ea98352b.svg"
+  },
+  {
+    "revision": "f728b337fb31c548682caece35ced4ea",
+    "url": "/jfelici5.github.io/afterhours/static/media/upcoming.f728b337.JPG"
+  },
+  {
+    "revision": "f93439e73b4020b4105955805bee1601",
+    "url": "/jfelici5.github.io/afterhours/static/media/zoe-emma.f93439e7.jpg"
+  },
+  {
+    "revision": "042c0dc8d7498632bd2aa5212da4b5e3",
+    "url": "/jfelici5.github.io/afterhours/static/media/zoe.042c0dc8.jpeg"
+  }
+]);
