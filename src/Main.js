4e3c6f745b992867368b65643be83219784dfc9c
jfelici5 <jfelici5@u.rochester.edu>
2020-08-04 22:45:39 -0700

    added collapsed navbar

--- src/Main.js
+++ src/Main.js
@@ -8,7 +8,11 @@ import './CSS/Videos.css'
 import logo from './imagesmisc/finallogopng.png';
 import $ from "jquery";
 import {Route, Switch} from 'react-router-dom'
+import {Navbar, Nav} from 'react-bootstrap'
 import {Gallery} from './Gallery'
+import './CSS/Navbar.css'
+
+
 
 export default function Main(){
   
@@ -26,8 +30,10 @@ export default function Main(){
 
 		$('.scroller').each(function(i) {
 				if ($(this).position().top <= scrollDistance +10) {
-						$('a').removeClass('active');
-						$('a').eq(i).addClass('active');
+						$('.et-hero-tabs-container a').removeClass('active');
+            $('.et-hero-tabs-container a').eq(i).addClass('active');
+            $('.bsnavbar a').removeClass('active');
+						$('.bsnavbar a').eq(i).addClass('active');
 				}
 		});
   }).scroll();
@@ -36,10 +42,29 @@ export default function Main(){
     return(
 
 <div data-spy="scroll" data-target=".nav">
-<div className = "scroller">          
+
+<div className = "scroller"> 
+<Navbar className = "bsnavbar" sticky = "top" expand="lg">
+  <Navbar.Toggle className = "nav-conts"/>
+  <Navbar.Collapse className = ".collapse"id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/#"data-toggle="collapse"data-target=".navbar-collapse.show">Home</Nav.Link>
+      <Nav.Link className = "link-text"href="/#about"data-toggle="collapse"data-target=".navbar-collapse.show">About</Nav.Link>
+      <Nav.Link className = "link-text"href="/#upcoming"data-toggle="collapse"data-target=".navbar-collapse.show">Upcoming</Nav.Link>
+      <Nav.Link className = "link-text"href="/#members"data-toggle="collapse"data-target=".navbar-collapse.show">Members</Nav.Link>
+      <Nav.Link className = "link-text"href="/#music"data-toggle="collapse"data-target=".navbar-collapse.show">Music</Nav.Link>
+      <Nav.Link className = "link-text" href="/#media"data-toggle="collapse"data-target=".navbar-collapse.show">Media</Nav.Link>
+      <Nav.Link className = "link-text"href="/gallery"data-toggle="collapse"data-target=".navbar-collapse.show">Gallery</Nav.Link>
+      <Nav.Link className = "link-text" href="https://docs.google.com/forms/d/e/1FAIpQLSeUO7YjUXzZ5q3gLureYPazT-AXDPB2MdF02i3I0leFc4jQFw/viewform?usp=sf_link" target="_blank">Book Us</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>         
   <section className="et-hero-tabs" id ='#'>
+
     <nav className = "container-holder" id = "top-menu">
+    
       <nav className="et-hero-tabs-container" id = "top-menu">
+      
         <a href = "#" className = "active">HOME</a>
         <a href="/#about">ABOUT</a>
         <a href="/#upcoming">SHOWS</a>
